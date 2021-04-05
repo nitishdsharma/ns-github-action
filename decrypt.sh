3a74e32807815a1cb8874401c2f9c7695779462d
Nitish Sharma <46995651+nitishdsharma@users.noreply.github.com>
2021-04-05 08:51:57 +0530

    Delete decrypt.sh

--- decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
