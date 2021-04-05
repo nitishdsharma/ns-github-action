a96a6d3cd634fc6a34bf6c271f0b3b56f5542898
Nitish Sharma <46995651+nitishdsharma@users.noreply.github.com>
2021-04-05 08:52:39 +0530

    Create decrypt_secret.sh

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/my_secret.json my_secret.json.gpg
